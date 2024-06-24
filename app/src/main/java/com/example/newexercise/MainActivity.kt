e3db5d650455f9ebd610f6af0a77f6112467b70f
IvanPS1 <ivan-pankratov43@mail.ru>
2024-06-24 09:42:48 +0300

    Counter

--- app/src/main/java/com/example/newexercise/MainActivity.kt
+++ app/src/main/java/com/example/newexercise/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.newexercise
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private var a: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +19,14 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val button: Button = findViewById(R.id.button)
+        val textView: TextView = findViewById(R.id.textView2)
+
+        Toast.makeText(this, "Tap to Click", Toast.LENGTH_LONG).show()
+
+        button.setOnClickListener{
+           textView.text = "${a++}"
         }
     }
 }
